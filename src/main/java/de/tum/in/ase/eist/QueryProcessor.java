258ecc77705c370707ec818d8c7fb0b37d8032b1
刘明希 <mingxi.liu@tum.de>
2022-07-14 15:09:27 +0200

    rename myself

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -8,11 +8,11 @@ public class QueryProcessor {
     public String process(String query) {
 		query = query.toLowerCase();
         if (query.contains("shakespeare")) {
-            return "William Shakespeare (26 April 1964 - 23 April 2016) was an " +
+            return "William Shakespeare (26 April 1964 - 23 April 2116) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "penguin";
         } else { // TODO extend the programm here
             return "";
         }
