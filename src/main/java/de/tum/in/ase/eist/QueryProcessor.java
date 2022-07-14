a161916557ba02bbc7c3a7c567fb1a964229de70
刘明希 <mingxi.liu@tum.de>
2022-07-14 15:27:02 +0200

    add many else ifs

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,14 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "penguin";
-        } else { // TODO extend the programm here
+        } else if (query.contains("age")){
+            return "2";
+        } else if (query.contains("characteristic")) {
+            return "stupid";
+        } else if (query.contains("friend")) {
+            return "pgdp";
+        }
+        else {
             return "";
         }
     }
